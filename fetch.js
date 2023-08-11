c96562d0905e02ba11dd22f7ca6fc111da2b5578
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-08-11 18:20:56 -0300

    progress meet sesion 11.08.2023

--- fetch.js
+++ fetch.js
@@ -1,5 +1,36 @@
-export const urlApi = async (url) => {
-    const response = await fetch(url)
-    const data = response.json()
-    return data
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "0087de59eceafec6ae96f68244d45721";
+const searchBox = document.querySelector('input');
+const searchBtn = document.querySelector('button');
+const displayTemperature = document.querySelector('.temperature');
+const displayIcon = document.querySelector('.icon')
+
+export async function checkWeather(city) {
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json()
+    displayTemperature.innerHTML = data.main.temp + "°C";
+    console.log("Temperature:" + " " + data.main.temp)
+    console.log(data)
+    console.log(data.main.temp)
+    console.log(data.main.temp_max)
+    console.log(data.main.temp_min)
+ //   console.log(data.weather[0].id) probando imagen
+
+
 }
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+    console.log('Displaying the weather of:' + ' ' + searchBox.value)
+
+})
+
+
+
+searchBox.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        checkWeather(searchBox.value);
+        console.log('Displaying the weather of:' + ' ' + searchBox.value)        
+}})
--- script.js
+++ script.js
@@ -1,55 +1,5 @@
+import { checkWeather } from "./fetch.js";
 
-
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const apiKey = "0087de59eceafec6ae96f68244d45721";
-const searchBox = document.querySelector('input');
-const searchBtn = document.querySelector('button');
-const prueba = document.querySelector('.parrafo');
-
-
-async function checkWeather(city) {
-
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json()
-    prueba.innerHTML = data.main.temp;
-    console.log("Temperature:" + " " + data.main.temp)
-    console.log(data)
-    console.log(data.main.temp)
-    console.log(data.main.temp_max)
-    console.log(data.main.temp_min)
-
-
-
-    const ctx = document.getElementById('myChart');
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: code,
-            datasets: [{
-                label: 'Valor del peso Chileno en diferentes cambios',
-                data: value,
-                borderWidth: 1
-
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-
-
-}
-
-searchBtn.addEventListener("click", () => {
-    checkWeather(searchBox.value);
-    console.log('Displaying the weather of:' + ' ' + searchBox.value)
-
-})
 
 
 
