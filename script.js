2feabf2e7fdaa10f5cbb81da144cd3b7cb5cd34f
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-08-10 22:07:48 -0400

    API definitiva

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-import { urlApi } from "./fetch.js";
 
-let code = [];
-let value = [];
 
-async function renderData() {
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "0087de59eceafec6ae96f68244d45721";
+const searchBox = document.querySelector('input');
+const searchBtn = document.querySelector('button');
+const prueba = document.querySelector('.parrafo');
 
-    const chileansCurrency = await urlApi("https://api.gael.cloud/general/public/clima")
-    code = chileansCurrency.map(chileanCurrency => chileanCurrency.Estacion)
-    value = chileansCurrency.map(chileanCurrency => chileanCurrency.Temp)
-    console.log(value)
+
+async function checkWeather(city) {
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json()
+    prueba.innerHTML = data.main.temp;
+    console.log("Temperature:" + " " + data.main.temp)
+    console.log(data)
+    console.log(data.main.temp)
+    console.log(data.main.temp_max)
+    console.log(data.main.temp_min)
+
+
+
     const ctx = document.getElementById('myChart');
 
     new Chart(ctx, {
@@ -30,6 +41,17 @@ async function renderData() {
             }
         }
     });
+
+
 }
 
-renderData()
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+    console.log('Displaying the weather of:' + ' ' + searchBox.value)
+
+})
+
+
+
+
+
