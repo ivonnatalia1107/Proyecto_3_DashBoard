340f8d9b404a8e1e7026cecf6a6b5bbbd6c07640
Dreeyas <a.navarretegarrido@gmail.com>
2023-08-15 13:10:20 -0400

    add climate cards at fetch2 with styles

--- fetch2.js
+++ fetch2.js
@@ -2,6 +2,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=
 const apiKey = "0087de59eceafec6ae96f68244d45721";
 const searchBox = document.querySelector('input');
 const searchBtn = document.querySelector('button');
+const cardContainer = document.querySelector('.card-container')
 
 export async function checkWeatherForDays(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -9,8 +10,56 @@ export async function checkWeatherForDays(city) {
 
    console.log(data) 
 
+   ///creado el container
 
-}
+   cardContainer.innerHTML = '';
+
+   const forecasts = data.list;
+
+    for (let i = 0; i < forecasts.length; i += 8) {
+        const forecast = forecasts[i];
+    
+        const date = new Date(forecast.dt * 1000);
+        const dayOfWeek = date.toLocaleString('en-US', { weekday: 'short' });
+        const temperature = forecast.main.temp;
+        const weatherCondition = forecast.weather[0].main; 
+        const roundedTemperature = Math.round(temperature);
+        const card = document.createElement('div');
+        card.classList.add('weather-card');
+    
+        const iconImg = document.createElement('img');
+        
+        if (weatherCondition === "Clouds") {
+            iconImg.src = "images/clouds.png";
+        }
+        else if (weatherCondition === "Clear") {
+            iconImg.src = "images/clear.png";
+        }
+        else if (weatherCondition === "Rain") {
+            iconImg.src = "images/rain.png";
+        }
+        else if (weatherCondition === "Drizzle") {
+            iconImg.src = "images/drizzle.png";
+        }
+        else if (weatherCondition === "Mist") {
+            iconImg.src = "images/mist.png";
+    
+        }
+    
+        iconImg.alt = forecast.weather[0].description;
+    
+        const temperatureText = document.createElement('p');
+        temperatureText.textContent = `${roundedTemperature}°C`;
+    
+        const dayText = document.createElement('p');
+        dayText.textContent = dayOfWeek;
+    
+        card.appendChild(iconImg);
+        card.appendChild(temperatureText);
+        card.appendChild(dayText);
+    
+        cardContainer.appendChild(card);
+}}
 
 searchBtn.addEventListener("click", () => {
     checkWeatherForDays(searchBox.value);
